e692c15fac85341e6892d2b2d4ce26c258fd075c
Robb, Sofia <sofia2341@gmail.com>
2018-03-01 16:38:20 -0700

    cmd typo

--- scripts/example.pipeline.py
+++ scripts/example.pipeline.py
@@ -7,7 +7,7 @@ countcmd = 'grep \'hits found\' test.blastout.tab  | perl -ne \'m/(\d+)/; $count
 
 blastcmd_run = subprocess.run(blastcmd, shell=True , stdout = subprocess.PIPE, stderr=subprocess.PIPE)
 if blastcmd_run.returncode == 0:
-  countcmd_run = subprocess.run(countcomd,  shell=True , stdout = subprocess.PIPE, stderr=subprocess.PIPE)
+  countcmd_run = subprocess.run(countcmd,  shell=True , stdout = subprocess.PIPE, stderr=subprocess.PIPE)
 else:
    sys.exit("BLAST had issues " + blastcmd_run.stderr.decode('utf-8'))
 
