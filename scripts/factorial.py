a7914820264b88ecae3ec0a4905b36cfaa01bfd3
Konferenz Teilnehmer (k00319245) <k00319245@evop2018login.imp.fu-berlin.de>
2018-03-09 21:59:01 +0100

    adding dict of dict script

---
+++ scripts/factorial.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+count=5
+result=1
+
+while count > 0:
+    result*=count # result = result * count
+    count-=1 # count = count -1
+print(result)
+
+###### do it again ####
+print("lets do it again\n")
+count = 1
+result = 1
+while count <= 5:
+    result*=count
+    count+=1
+print(result)
--- scripts/fasta_datastucture.py
+++ scripts/fasta_datastucture.py
@@ -42,7 +42,31 @@ for line in file:
 file.close()
 
 ## Now you have a dictionary of all your sequences
+seq_data = {}
+
 print(seqs)
 for id in seqs:
   seq = seqs[id]
-  print(id,seq)
+  a_count = seq.count('A')
+  t_count = seq.count('T')
+  g_count = seq.count('G')
+  c_count = seq.count('C')
+  
+  # we need to declare that the value of key->id is of type dictionary
+  seq_data[id] = {}
+
+  # we need to declare that the value of key->nt_comp is of type dictionary
+  seq_data[id]['nt_comp'] = {}
+  # adding keys to our nt_comp dict
+  seq_data[id]['nt_comp']['A']= a_count
+  seq_data[id]['nt_comp']['T']= t_count
+  seq_data[id]['nt_comp']['G']= g_count
+  seq_data[id]['nt_comp']['C']= c_count
+
+  # we can just add a value to this key->seqlen
+  seq_data[id]['seqlen'] = len(seq)
+  
+  # we can just add a value to this key->seq
+  seq_data[id]['seq'] = seq
+
+print(seq_data)
