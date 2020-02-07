504534080f7a84cacb2bd1215aaa7af44ebfddaf
Sofia Robb <sofia2341@gmail.com>
2020-02-07 11:43:42 -0700

    adding this long ago updated file

--- TOC/create_TOC.sh
+++ TOC/create_TOC.sh
@@ -1,5 +1,5 @@
 
 
 head ../README.md > tmp
-./gh-md-toc ../EVOP_Python_2019.md |  perl -pe 's/\(#/\(EVOP_Python_2018.md\/#/' >> tmp
+./gh-md-toc ../EVOP_Python_2019.md |  perl -pe 's/\(#/\(EVOP_Python_2019.md\/#/' >> tmp
 mv tmp ../README.md
